fd8b1da258c14a072b360cce53fdc6a9ae07e0d5
Jan Starý <hans@stare.cz>
2025-06-10 11:06:46 +0200

    recognize -C and -G

--- life.c
+++ life.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <unistd.h>
 
 int bflag = 1;
@@ -5,17 +6,27 @@ int cflag = 0;
 int gflag = 0;
 int kflag = 0;
 
+uint64_t backgen = 0;
+uint64_t stopgen = 0;
+
 int
 main(int argc, char** argv)
 {
 	int c;
+	const char* e = NULL;
 
-	while ((c = getopt(argc, argv, "bcgk")) != -1) switch (c) {
+	while ((c = getopt(argc, argv, "bC:cG:gk")) != -1) switch (c) {
 		case 'b':
 			bflag = 1;
 			break;
+		case 'C':
+			backgen = strtonum(optarg, 1, 16, &e);
+			break;
 		case 'c':
 			cflag = 0;
+			break;
+		case 'G':
+			stopgen = strtonum(optarg, 1, UINT64_MAX, &e);
 			break;
 		case 'g':
 			gflag = 0;
