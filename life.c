69295184cf4f05bd19b529d415e41bda50a46df8
Jan Starý <hans@stare.cz>
2025-06-10 10:55:53 +0200

    global flags and getopt()

--- life.c
+++ life.c
@@ -1,5 +1,29 @@
+#include <unistd.h>
+
+int bflag = 1;
+int cflag = 0;
+int gflag = 0;
+int kflag = 0;
+
 int
 main(int argc, char** argv)
 {
+	int c;
+
+	while ((c = getopt(argc, argv, "bcgk")) != -1) switch (c) {
+		case 'b':
+			bflag = 1;
+			break;
+		case 'c':
+			cflag = 0;
+			break;
+		case 'g':
+			gflag = 0;
+			break;
+		case 'k':
+			kflag = 0;
+			break;
+	}
+
 	return 0;
 }
